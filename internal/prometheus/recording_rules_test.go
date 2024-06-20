bfebb379a71c5451e05a8ca773c9afe1c85b15c0
Kristijonas Leipus <kristijonas.leipus@vinted.com>
2024-06-20 13:55:26 +0300

    Add Prometheus rule generators for metrics aggregated by sloth ID (#2)
    To be used when SLO has one metrics series and some labels change, to continue the same series in the recording rules. Ignore all other labels except for sloth_id.

--- internal/prometheus/recording_rules_test.go
+++ internal/prometheus/recording_rules_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/slok/sloth/internal/alert"
+	"github.com/slok/sloth/internal/app/generate"
 	"github.com/slok/sloth/internal/info"
 	"github.com/slok/sloth/internal/prometheus"
 )
@@ -187,6 +188,116 @@ func TestGenerateSLIRecordingRules(t *testing.T) {
 				{
 					Record: "slo:sli_error:ratio_rate30d",
 					Expr:   "sum_over_time(slo:sli_error:ratio_rate5m{sloth_id=\"test\", sloth_service=\"test-svc\", sloth_slo=\"test-name\"}[30d])\n/ ignoring (sloth_window)\ncount_over_time(slo:sli_error:ratio_rate5m{sloth_id=\"test\", sloth_service=\"test-svc\", sloth_slo=\"test-name\"}[30d])\n",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+						"sloth_window":  "30d",
+					},
+				},
+			},
+		},
+
+		"Having an SLO with SLI(events) and its mwmb alerts should create the recording rules aggregated by sloth id.": {
+			generator: func() generator { return prometheus.OptimizedBySlothIDSLIRecordingRulesGenerator },
+			slo: prometheus.SLO{
+				ID:         "test",
+				Name:       "test-name",
+				Service:    "test-svc",
+				TimeWindow: 30 * 24 * time.Hour,
+				SLI: prometheus.SLI{
+					Events: &prometheus.SLIEvents{
+						ErrorQuery: `rate(my_metric[{{.window}}]{error="true"})`,
+						TotalQuery: `rate(my_metric[{{.window}}])`,
+					},
+				},
+				Labels: map[string]string{
+					"kind": "test",
+				},
+			},
+			alertGroup: getAlertGroup(),
+			expRules: []rulefmt.Rule{
+				{
+					Record: "slo:sli_error:ratio_rate5m",
+					Expr:   "(rate(my_metric[5m]{error=\"true\"}))\n/\n(rate(my_metric[5m]))\n",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+						"sloth_window":  "5m",
+					},
+				},
+				{
+					Record: "slo:sli_error:ratio_rate30m",
+					Expr:   "(rate(my_metric[30m]{error=\"true\"}))\n/\n(rate(my_metric[30m]))\n",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+						"sloth_window":  "30m",
+					},
+				},
+				{
+					Record: "slo:sli_error:ratio_rate1h",
+					Expr:   "(rate(my_metric[1h]{error=\"true\"}))\n/\n(rate(my_metric[1h]))\n",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+						"sloth_window":  "1h",
+					},
+				},
+				{
+					Record: "slo:sli_error:ratio_rate2h",
+					Expr:   "(rate(my_metric[2h]{error=\"true\"}))\n/\n(rate(my_metric[2h]))\n",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+						"sloth_window":  "2h",
+					},
+				},
+				{
+					Record: "slo:sli_error:ratio_rate6h",
+					Expr:   "(rate(my_metric[6h]{error=\"true\"}))\n/\n(rate(my_metric[6h]))\n",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+						"sloth_window":  "6h",
+					},
+				},
+				{
+					Record: "slo:sli_error:ratio_rate1d",
+					Expr:   "(rate(my_metric[1d]{error=\"true\"}))\n/\n(rate(my_metric[1d]))\n",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+						"sloth_window":  "1d",
+					},
+				},
+				{
+					Record: "slo:sli_error:ratio_rate3d",
+					Expr:   "(rate(my_metric[3d]{error=\"true\"}))\n/\n(rate(my_metric[3d]))\n",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+						"sloth_window":  "3d",
+					},
+				},
+				{
+					Record: "slo:sli_error:ratio_rate30d",
+					Expr:   "sum by (sloth_id) (sum_over_time(slo:sli_error:ratio_rate5m{sloth_id=\"test\"}[30d]))\n/\nsum by (sloth_id) (count_over_time(slo:sli_error:ratio_rate5m{sloth_id=\"test\"}[30d]))\n",
 					Labels: map[string]string{
 						"kind":          "test",
 						"sloth_service": "test-svc",
@@ -506,6 +617,7 @@ func TestGenerateSLIRecordingRules(t *testing.T) {
 
 func TestGenerateMetaRecordingRules(t *testing.T) {
 	tests := map[string]struct {
+		generator  generate.MetadataRecordingRulesGenerator
 		info       info.Info
 		slo        prometheus.SLO
 		alertGroup alert.MWMBAlertGroup
@@ -513,6 +625,7 @@ func TestGenerateMetaRecordingRules(t *testing.T) {
 		expErr     bool
 	}{
 		"Having and SLO an its mwmb alerts should create the metadata recording rules.": {
+			generator: prometheus.MetadataRecordingRulesGenerator,
 			info: info.Info{
 				Version: "test-ver",
 				Mode:    info.ModeTest,
@@ -612,13 +725,114 @@ slo:error_budget:ratio{sloth_id="test", sloth_service="test-svc", sloth_slo="tes
 				},
 			},
 		},
+		"Having and SLO an its mwmb alerts should create the metadata recording rules aggregated by sloth id.": {
+			generator: prometheus.MetadataRecordingRulesBySlothIDGenerator,
+			info: info.Info{
+				Version: "test-ver",
+				Mode:    info.ModeTest,
+				Spec:    "test/v1",
+			},
+			slo: prometheus.SLO{
+				ID:         "test",
+				Name:       "test-name",
+				Service:    "test-svc",
+				Objective:  99.9,
+				TimeWindow: 30 * 24 * time.Hour,
+				Labels: map[string]string{
+					"kind": "test",
+				},
+			},
+			alertGroup: getAlertGroup(),
+			expRules: []rulefmt.Rule{
+				{
+					Record: "slo:objective:ratio",
+					Expr:   "vector(0.9990000000000001)",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+					},
+				},
+				{
+					Record: "slo:error_budget:ratio",
+					Expr:   "vector(1-0.9990000000000001)",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+					},
+				},
+				{
+					Record: "slo:time_period:days",
+					Expr:   "vector(30)",
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+					},
+				},
+				{
+					Record: "slo:current_burn_rate:ratio",
+					Expr: `max(slo:sli_error:ratio_rate5m{sloth_id="test"})
+/
+max(slo:error_budget:ratio{sloth_id="test"})
+`,
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+					},
+				},
+				{
+					Record: "slo:period_burn_rate:ratio",
+					Expr: `max(slo:sli_error:ratio_rate30d{sloth_id="test"})
+/
+max(slo:error_budget:ratio{sloth_id="test"})
+`,
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+					},
+				},
+				{
+					Record: "slo:period_error_budget_remaining:ratio",
+					Expr:   `1 - max(slo:period_burn_rate:ratio{sloth_id="test"})`,
+					Labels: map[string]string{
+						"kind":          "test",
+						"sloth_service": "test-svc",
+						"sloth_slo":     "test-name",
+						"sloth_id":      "test",
+					},
+				},
+				{
+					Record: "sloth_slo_info",
+					Expr:   `vector(1)`,
+					Labels: map[string]string{
+						"kind":            "test",
+						"sloth_service":   "test-svc",
+						"sloth_slo":       "test-name",
+						"sloth_id":        "test",
+						"sloth_version":   "test-ver",
+						"sloth_mode":      "test",
+						"sloth_spec":      "test/v1",
+						"sloth_objective": "99.9",
+					},
+				},
+			},
+		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			assert := assert.New(t)
 
-			gotRules, err := prometheus.MetadataRecordingRulesGenerator.GenerateMetadataRecordingRules(context.TODO(), test.info, test.slo, test.alertGroup)
+			gotRules, err := test.generator.GenerateMetadataRecordingRules(context.TODO(), test.info, test.slo, test.alertGroup)
 
 			if test.expErr {
 				assert.Error(err)
